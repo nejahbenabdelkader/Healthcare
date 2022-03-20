cd139b8d72ab2685804ff20d9190ad0fcdf992cf
nejahbenabdelkader <70769535+nejahbenabdelkader@users.noreply.github.com>
2022-03-20 11:36:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from  'react-router-dom';
+
+
+import Home from './pages';
+import SignupPage from './pages/signup';
+import SigninPage from './pages/signin';
+import TimeTable from './components/TimeTable';
+import DoctorData from './components/Doctor/Data/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /*<Router>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />
+        <Route path='/signup' component={SignupPage} exact />
+      </Switch>
+    </Router>*/
+    <DoctorData />
   );
 }
 
